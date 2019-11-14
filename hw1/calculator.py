be30a10b79234926fbd4ec1938bf6db373cca0a6
Jane <grindevald666@gmail.com>
2019-11-14 14:58:57 +0300

    Changing print

--- hw1/calculator.py
+++ hw1/calculator.py
@@ -1,25 +1,25 @@
 def add(x : int, y : int) -> int:
-    print(x + y)
+    return x + y
 
 
 def sub(x : int, y : int) -> int:
-    print(x - y)
+    return x - y
 
 
 def mult(x : int, y : int) -> int:
-    print(x * y)
+    return x * y
 
 
 def div(x : int, y : int) -> int:
-    print(4 / 2)
+    return 4 / 2
 
 
 def mod(x : int, y : int) -> int:
-    print(x % y)
+    return x % y
 
 
 def pow(x : int, y : int) -> int:
-    print(x ^ y)
+    return x ^ y
 
 
 if __name__ == '__main__':
