497586ef9c47caae39ab045e5f838165c6cceaa3
Sasha Vorobeva <avorobdm@gmail.com>
2019-11-14 14:46:21 +0300

    Implement sub

--- hw1/calculator.py
+++ hw1/calculator.py
@@ -2,8 +2,8 @@ def add(x: int, y: int) -> int:
     return x + y
 
 
-def sub(x : int, y : int) -> int:
-    pass
+def sub(x: int, y: int) -> int:
+    return x - y
 
 
 def mult(x : int, y : int) -> int:
