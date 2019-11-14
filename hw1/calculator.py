1863958510c63bdf92e1740715cac101cd11fe9d
Kate <ekaterinaicenko@gmail.com>
2019-11-14 14:59:40 +0300

    Changed main

--- hw1/calculator.py
+++ hw1/calculator.py
@@ -23,9 +23,9 @@ def pow(x : int, y : int) -> int:
 
 
 if __name__ == '__main__':
-    print(add(4, 5))
-    print(sub(3, 1))
-    print(mult(2, 2))
-    print(div(4, 2))
-    print(mod(5, 3))
-    print(pow(6, 2))
+    add(4, 5)
+    sub(3, 1)
+    mult(2, 2)
+    div(4, 2)
+    mod(5, 3)
+    pow(6, 2)
